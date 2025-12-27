e20965af3e12a7a3af307ac6a79340ed9733aba1
Albaraa Hasan <albaraa456@GMAIL.COM>
2025-12-27 20:35:34 +0300

    wrap inside a folder

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
       formats: ["es"],
       fileName: (format, entryName) => `${entryName}.js`,
     },
-    outDir: "assets",
+    outDir: "theme/assets",
     rollupOptions: {
       output: {
         manualChunks: undefined,
